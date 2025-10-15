c058904e7b213b8a878b44f13fc06e6730c3cf14
m9iv <aethawalka@gmail.com>
2025-10-15 11:49:51 +0300

    Add button styles

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,31 +1,31 @@
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom"
-import { createOrder } from "../../services/apiRestaurant"
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
+import { createOrder } from '../../services/apiRestaurant'
 // import { useState } from "react"
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   )
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -37,7 +37,7 @@ function CreateOrder() {
   const cart = fakeCart
 
   const navigation = useNavigation()
-  const isSubmitting = navigation.state === "submitting"
+  const isSubmitting = navigation.state === 'submitting'
 
   const formErrors = useActionData()
 
@@ -85,8 +85,11 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Place order..." : "Order now"}
+          <button
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Place order...' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -101,21 +104,23 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   }
 
   // Errors handling
   const errors = {}
   if (!isValidPhone(order.phone))
     errors.phone =
-      "Please give us your correct phone number. We might need it to contact you."
+      'Please give us your correct phone number. We might need it to contact you.'
 
   if (Object.keys(errors).length > 0) return errors
 
   // New order logic and redirect if success
-  const newOrder = await createOrder(order)
+  // const newOrder = await createOrder(order)
 
-  return redirect(`/order/${newOrder.id}`)
+  // return redirect(`/order/${newOrder.id}`)
+
+  return null
 }
 
 export default CreateOrder
